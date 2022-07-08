155c7af0c890e6a62383414b582ae19e314658ce
gabisegger <gabi.segger@gmail.com>
2022-07-08 07:07:46 -0300

    feat: slicks on fullbanner and products

--- parte1/js/script.js
+++ parte1/js/script.js
@@ -1,1 +1,15 @@
 /* Coloque o JS neste arquivo */
+
+$('.block-1').slick({
+    dots: false,
+    infinite: true
+});
+
+$('.products').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplaySpeed: 2000
+});
